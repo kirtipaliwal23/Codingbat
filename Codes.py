37709a31d7c7d47d8862816ed48103f6075f40c7
kirtipaliwal23 <kirtipaliwal2323@gmail.com>
2020-08-05 14:58:39 +0530

    Latest

--- Codes.py
+++ Codes.py
@@ -1,4 +1,21 @@
-def make_tags(tag, word):
-  R="<"+tag+">"+word+"</"+tag+">"
-  return R
-make_tags("K","Kirti")
+#Make tags
+# def make_tags(tag, word):
+#   R="<"+tag+">"+word+"</"+tag+">"
+#   return R
+# make_tags("K","Kirti")
+
+# String-1 > make_out_word
+
+# def make_out_word(out, word):
+#   L=out[:2]+word+out[2:]
+#   return L
+# make_out_word("[[]]","Kirti")
+
+#extra_end
+
+# def extra_end(str):
+#   if len(str)>=2:
+#     S=str[len(str)-2:len(str)]
+#     N=3*S
+#     return N
+# extra_end("Delhi")
